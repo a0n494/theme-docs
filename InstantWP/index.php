193a044bb889694df1be86bb0dd4635dcf2d7e51
Zach Engstrom <Zach.Engstrom@bluecrossmn.com>
2015-06-16 03:33:51 -0500

    most recent 2015/06/16

--- InstantWP/index.php
+++ InstantWP/index.php
@@ -9,17 +9,18 @@ $version = $config['version'];
 $debugger = $config['debugger'];
 $border = '1px solid rgba(0, 0, 0, 0.2)'; 
 $serverInfo = $_SERVER['HTTP_USER_AGENT']; 
-if(preg_match('/\.NET/i',$serverInfo)) 
-{ 
+if(preg_match('/\.NET/i',$serverInfo)) { 
   $screenType = 'pc-window'; 
+} else {
+  $screenType = 'browser-window'; 
 }
 ?>
-<!DOCTYPE html>
-<html>
+<!DOCTYPE <html>
+<html class="<?php echo $screenType; ?>">
 <head>
 	<title>InstantWP - Your Local WordPress Installation</title>
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-	<link rel='stylesheet' href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<link rel='stylesheet' href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link href="instantwp.css" rel="stylesheet" type="text/css" />
 	<SCRIPT LANGUAGE="JavaScript">
 	<!--
@@ -36,68 +37,70 @@ if(preg_match('/\.NET/i',$serverInfo))
 	// End -->
 	</script>
 </head>
-<body class="<?php echo $screenType; ?>">
+<body class="">
 	<div id="container" class="container">
 		<div id="container-header" class="col-sm-8 col-sm-offset-2">
 			<div id="container-name"><img src="images/logo.gif" alt="logo" border="0" /></div>
 		</div>
-		<div class="input col-sm-8 col-sm-offset-2">
-			<b>Local Wordpress URL: </b>
-			<input class="url_textbox" id="<?= $install_dir ?>" name="<?= $install_dir ?>" type="text" size='30'
-			value='http://<?= $base_url ?>/<?= $install_dir ?>/'>&nbsp;
-			<input class="url_button" onclick="copyit('<?= $install_dir ?>', '<?= $install_dir ?>')"
-			type="button" value="Copy WordPress URL to Clipboard" name="copy_button">
+		<div class="copy-url col-sm-8 col-sm-offset-2">
+			<label for="<?= $install_dir ?>">Local Wordpress URL:</label>
+			<input type="text" id="<?= $install_dir ?>" placeholder="Search for..." name="<?= $install_dir ?>" value="http://<?= $base_url ?>/<?= $install_dir ?>/">
+			<button type="button" onclick="copyit('<?= $install_dir ?>', '<?= $install_dir ?>')" name="copy_button" style="">Copy WordPress URL to Clipboard</button>
 		</div>
 		<div class="wrapper col-sm-8 col-sm-offset-2">
 			<hr>
 			<div class="wrap_dirs">
 				<div class="wrap_dir col-sm-6">
 					<div class="dir">
-						<a href="http://<?= $base_url ?>/<?= $install_dir ?>" target="_blank"><i class="fa fa-home"></i> WordPress FrontPage</a>
+						<a href="http://<?= $base_url ?>/<?= $install_dir ?>" target="_blank"><i class="fa fa-home"></i>WordPress FrontPage</a>
 					</div>
 				</div>
 				<div class="wrap_dir col-sm-6">
 					<div class="dir">
-						<a href="http://<?= $base_url ?>/<?= $install_dir ?>/wp-login.php" target="_blank"><i class="fa fa-cog"></i> WordPress Admin</a>
+						<a href="http://<?= $base_url ?>/<?= $install_dir ?>/wp-login.php" target="_blank"><i class="fa fa-cog"></i>WordPress Admin</a>
 					</div>
 				</div>
 				<div class="wrap_dir col-sm-6">
 					<div class="dir">
-						<a href="http://<?= $base_url ?>/instantwp/externals/explore_plugins.php?wp=<?= $install_dir ?>"><i class='fa fa-folder-o'></i> Plugins Folder</a>
+						<a href="http://<?= $base_url ?>/instantwp/externals/explore_plugins.php?wp=<?= $install_dir ?>"><i class='fa fa-folder-o'></i>Plugins Folder</a>
 					</div>
 				</div>
 				<div class="wrap_dir col-sm-6">
 					<div class="dir">
-						<a href="http://<?= $base_url ?>/instantwp/externals/explore_themes.php?wp=<?= $install_dir ?>"><i class='fa fa-folder-o'></i> Themes Folder</a>
+						<a href="http://<?= $base_url ?>/instantwp/externals/explore_themes.php?wp=<?= $install_dir ?>"><i class='fa fa-folder-o'></i>Themes Folder</a>
 					</div>
 				</div>
 				<div class="wrap_dir col-sm-6">
 					<div class="dir">
-						<a href="http://<?= $base_url ?>/phpmyadmin" target="_blank"><i class="fa fa-database"></i> MySQL Admin</a>
+						<a href="http://<?= $base_url ?>/phpmyadmin" target="_blank"><i class="fa fa-database"></i>MySQL Admin</a>
 					</div>
 				</div>
 				<div class="wrap_dir col-sm-6">
 					<div class="dir">
-						<a href="http://www.instantwp.com/documentation/?v=<?=str_replace(" ", "", $version)?>" target="_blank"><i class="fa fa-info-circle"></i> Documentation</a>
+						<a href="http://www.instantwp.com/documentation/?v=<?=str_replace(" ", "", $version)?>" target="_blank"><i class="fa fa-info-circle"></i>Documentation</a>
 					</div>
 				</div>
 			</div>
 			<? if ( $config['show_additional_buttons'] == '1') {  ?>
 			<p class="col-sm-12">Please check out the <a target="_blank"  href="http://www.instantwp.com/documentation/faq/">FAQ</a> and <a target="_blank"  href="http://www.instantwp.com/unleashed/">Deployment Guide - Instant WordPress Unleashed</a> or some recommended <a target="_blank"  href="http://www.instantwp.com/wordpress-resources/">WordPress Resources</a></p>
 			<? } ?>
+			<div class="col-sm-12 creds">
+				<p class="col-sm-12 text-center"><b>Default Credentials</b></p>
+				<p class="col-sm-6 text-center"><u>WordPress</u><br>User = admin<br>Pass = password</p>
+				<p class="col-sm-6 text-center"><u>MySQL</u><br>User = root<br>Pass =</p>
+			</div>
 		</div>
 		<div class="wrapper col-sm-8 col-sm-offset-2">
-			<hr>
 			<div class="dir_title text-center"><h2>Other Projects</h2></div>
 			<div class="wrap_dirs">			
 			<?php
 				$dir = '../';
 					foreach(scandir($dir) as $entry) {
-						if ($entry != 'wordpress' && $entry != 'instantwp' && preg_match("/^[a-zA-Z]*$/", $entry)) {
-							echo "<div class='wrap_dir col-sm-6'><div class='dir'><a href='$dir$entry' target='_blank'><i class='fa fa-folder-o'></i> " . ucwords(str_replace(array('_','-'), ' ', $entry)) . "</a></div></div>";
+						if ($entry != 'wordpress' && $entry != 'instantwp' && !preg_match("/(.*\..*)/", $entry)) {
+							echo "<div class='wrap_dir col-sm-6'><div class='dir'><a href='$dir$entry' target='_blank'><i class='fa fa-folder-o'></i>" . ucwords(str_replace(array('_','-'), ' ', $entry)) . "</a></div></div>";
 						}
 					}
-					echo "<div class='wrap_dir col-sm-12 no-projects'><div class='dir'><a href='#none'><i class='fa fa-ban'></i> None</a></div></div>";
+					echo "<div class='wrap_dir col-sm-12 no-projects'><div class='dir'><a href='#none'><i class='fa fa-ban'></i>None</a></div></div>";
 			?>
 			</div>
 			<div class="footer col-sm-12">
@@ -108,16 +111,15 @@ if(preg_match('/\.NET/i',$serverInfo))
 			</div>
 		</div>
 	</div> <!-- END container -->
-	<script src='//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js'></script>
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js'></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	<script>
 		$(function() {
-			if ($('.wrap_dirs:last-child .dir').length == 0) {
+			if ($('.dir').length == 7) {
 				$('.no-projects').show();
 			};
+			$('.browser-window .fa').after('&nbsp;&nbsp;');
 		});
-	</script>
-	<script>
 	</script>
 </body>
 </html>
--- InstantWP/readme.md
+++ InstantWP/readme.md
@@ -1,21 +1,41 @@
 # Customizing InstantWP
 
-
-
 ### Install
 1. Create a folder called `InstantWP` in your Downloads folder (`C:\Users\a0n###\Downloads\`)
 2. Download InstantWP [here](http://www.instantwp.com/) and save it to `C:\Users\a0n###\Downloads\InstantWP`
 3. Create a folder called `bcbsmn-wp_template` where you keep your other local repositories (Example: `C:\Users\a0n###\path\to\local\repos\`)
 4. Double click `InstantWP_4.4.2.exe` that you downloaded in step #2
-5. Install InstantWP to the folder you created in step #3 (`C:\Users\a0n494\path\to\repos\bcbsmn-wp_template`) by clicking <button>Browse</button>
+5. Click <button>Browse</button> to install InstantWP to the folder you created in step #3 (`C:\Users\a0n###\path\to\local\repos\bcbsmn-wp_template`)
 6. Click <button>Install</button>
-7. Instant WordPress will be installed into the selected folder.
+7. Instant WordPress will be installed into the folder you selected in step #5
+8. Click <button>Close</button> once the installation is complete
 - **Note**:
 	- Instant WordPress does not add anything to your systems registry or otherwise. It is entirely portable and runs from with the folder where it is installed.
 
 ---
 
 ### Modifying Your Installation
-1. Browse to `C:\Users\a0n494\path\to\repos\bcbsmn-wp_template\iwpserver\htdocs`
-2. Open `index.php` in your text editor and delete all of its contents
-3. Copy everything from [this](https://github.com/a0n494/theme-docs/blob/master/InstantWP/index.php) and paste it into the `index.php` file that you just cleared. Save your changes.
+1. Browse to `C:\Users\a0n###\path\to\local\repos\bcbsmn-wp_template\iwpserver\htdocs`
+2. Rename `index.php` to `index.php.bak` and `instantwp.css` to `instantwp.css.bak`
+3. Create a new `index.php` and `instantwp.css`
+4. Copy everything from <a href="https://github.com/a0n494/theme-docs/blob/master/InstantWP/index.php" target="_blank">here</a> and paste it into the `index.php` file that created in step #3. Save your changes.
+5. Copy everything from <a href="https://github.com/a0n494/theme-docs/blob/master/InstantWP/instantwp.css" target="_blank">here</a> and paste it into the `instantwp.css` file that created in step #3. Save your changes.
+6. In `C:\Users\a0n###\path\to\local\repos\bcbsmn-wp_template\iwpserver\htdocs` rename the folder `wordpress` to `wordpress.bak`
+
+---
+
+### Connecting to the Repository
+1. In `C:\Users\a0n###\path\to\local\repos\bcbsmn-wp_template\iwpserver\htdocs` create a folder named `wordpress`
+2. Open SourceTree
+3. Click the `Clone / New` button in the top toolbar
+4. In the `Source Path / URL` input enter `git@github.com:bluecrossmn/carecoordination.git`
+5. In the `Destination Path` input browse to `C:\Users\a0n###\path\to\local\repos\bcbsmn-wp_template\iwpserver\htdocs\wordpress`
+6. Click the <button>Clone</button> button
+
+---
+
+### Connecting to Your Local DataBase
+1. Open `C:\Users\a0n###\path\to\local\repos\bcbsmn-wp_template\iwpserver\htdocs\wordpress\wp-config.php` in your text editor
+2. Edit line 25 to `	define('DB_NAME', 'wordpress_bcbsmn');`
+3. Edit line 27 to `	define('DB_PASSWORD', '');`
+4. Edit line 41 to `	$table_prefix = 'wp_';`
